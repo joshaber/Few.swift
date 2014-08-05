ec04dfc289a0807c6160339c9f36e085e7ef4add
joshaber <joshaber@gmail.com>
2014-08-05 00:23:49 -0400

    First pass at Label.

---
+++ Few/Label.swift
@@ -0,0 +1,53 @@
+//
+//  Label.swift
+//  Few
+//
+//  Created by Josh Abernathy on 8/5/14.
+//  Copyright (c) 2014 Josh Abernathy. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+public class Label<S: Equatable>: Element<S> {
+	private var textField: NSTextField?
+
+	private var text: String
+	private var frame: NSRect
+
+	public init(frame: NSRect, text: String) {
+		self.frame = frame
+		self.text = text
+	}
+
+	// MARK: Element
+
+	public override func applyDiff(other: Element<S>) {
+		if textField == nil { return }
+
+		let otherLabel = other as Label
+		if text != otherLabel.text {
+			text = otherLabel.text
+			textField!.stringValue = text
+		}
+
+		if frame != otherLabel.frame {
+			frame = otherLabel.frame
+			textField!.frame = frame
+		}
+	}
+
+	public override func realize(component: Component<S>, parentView: NSView) {
+		textField = NSTextField(frame: frame)
+		textField!.editable = false
+		textField!.drawsBackground = false
+		textField!.bordered = false
+		textField!.stringValue = text
+
+		super.realize(component, parentView: parentView)
+	}
+
+	public override func getContentView() -> NSView? {
+		return textField
+	}
+}
