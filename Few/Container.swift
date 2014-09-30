bb9c5741be2706f6fe2f327640e4947a5275b260
joshaber <joshaber@gmail.com>
2014-09-29 21:08:16 -0400

    Another pass at a correct Container diff.

--- Few/Container.swift
+++ Few/Container.swift
@@ -25,44 +25,78 @@ public class Container: Element {
 	// MARK: Element
 
 	public override func applyDiff(other: Element) {
+		var newChildren = [Element]()
+
 		let otherContainer = other as Container
-		var newChildren = Array<Element>()
-		let myChildCount = children.count
-		let theirChildCount = otherContainer.children.count
-		for i in 0..<max(myChildCount, theirChildCount) {
-			var myChild: Element?
-			if i < myChildCount {
-				myChild = children[i]
+		let otherChildren = otherContainer.children
+
+		let myChildrenByKey = childrenByKey(children)
+		let theirChildrenByKey = childrenByKey(otherChildren)
+
+		var usedKeys = [String: Void]()
+
+		var childQueue = otherChildren
+
+		for child in children {
+			var match: Element?
+			// First try to find a match based on the key.
+			if let key = child.key {
+				match = theirChildrenByKey[key]
+				usedKeys[key] = ()
 			}
 
-			var theirChild: Element?
-			if i < theirChildCount {
-				theirChild = otherContainer.children[i]
+			// If that fails and we still have new children, use one of those.
+			if match == nil && childQueue.count > 0 {
+				match = childQueue[0]
+				childQueue.removeAtIndex(0)
+
+				// We already used this child when doing key matching, so don't
+				// use it again.
+				if let key = match!.key {
+					if usedKeys[key] != nil {
+						match = nil
+					}
+				}
 			}
 
-			if myChild != nil && theirChild != nil {
-				if myChild!.canDiff(theirChild!) {
-					myChild!.applyDiff(theirChild!)
-					newChildren.append(myChild!)
+			// If we have a match/pair then do the diff dance.
+			if let match = match {
+				if child.canDiff(match) {
+					child.applyDiff(match)
+					newChildren.append(child)
 				} else {
-					myChild!.derealize()
+					child.derealize()
 
 					let component: Component<Any>? = getComponent()
-					curry(theirChild!.realize) <^> component <*> containerView
-					newChildren.append(theirChild!)
+					curry(match.realize) <^> component <*> containerView
+					newChildren.append(match)
 				}
-			} else if let myChild = myChild {
-				myChild.derealize()
-			} else if let theirChild = theirChild {
-				let component: Component<Any>? = getComponent()
-				curry(theirChild.realize) <^> component <*> containerView
-				newChildren.append(theirChild)
+			} else {
+				// If we didn't find a match and ran out of new children, then 
+				// it must not exist anymore.
+				child.derealize()
 			}
-
-			children = newChildren
-
-			super.applyDiff(other)
 		}
+
+		// Anything left over at this point must be new.
+		for child in childQueue {
+			let component: Component<Any>? = getComponent()
+			curry(child.realize) <^> component <*> containerView
+			newChildren.append(child)
+		}
+
+		children = newChildren
+	}
+
+	private func childrenByKey(children: [Element]) -> [String: Element] {
+		var childrenByKey = [String: Element]()
+		for child in children {
+			if let key = child.key {
+				childrenByKey[key] = child
+			}
+		}
+
+		return childrenByKey
 	}
 
 	public override func realize<S>(component: Component<S>, parentView: NSView) {
@@ -89,6 +123,8 @@ public class Container: Element {
 	}
 }
 
+// This doesn't work because Swift gets confused about Few's Element and 
+// ArrayLiteralConvertible's Element.
 //extension Container: ArrayLiteralConvertible {
 //	public class func convertFromArrayLiteral(elements: Element...) -> Self {
 //		return Container(elements)
