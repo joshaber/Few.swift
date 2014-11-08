dc5908f5c8a24aec1ddb044125d1fdcdbb6fc5d7
joshaber <joshaber@gmail.com>
2014-11-08 11:22:31 -0500

    Use a nested component.

--- FewDemo/DemoComponent2.swift
+++ FewDemo/DemoComponent2.swift
@@ -10,23 +10,27 @@ import Foundation
 import Few
 
 struct DemoState2 {
+	let loggedIn: Bool = false
+	let logInState: LogInState
+}
+
+struct LogInState {
 	let username: String = ""
 	let password: String = ""
 }
 
-class DemoComponent2<S>: Few.Component<DemoState2> {
-	init() {
-		let initialState = DemoState2()
-		super.init(render: DemoComponent2.render, initialState: initialState)
+class LogInComponent<S>: Few.Component<LogInState> {
+	init(state: LogInState) {
+		super.init(render: LogInComponent.render, initialState: state)
 	}
 
-	class func render(component: Few.Component<DemoState2>, state: DemoState2) -> Element {
+	class func render(component: Few.Component<LogInState>, state: LogInState) -> Element {
 		let usernameField = Input(initialText: "", placeholder: "Username") { str in
-			component.replaceState(DemoState2(username: str, password: state.password))
+			component.replaceState(LogInState(username: str, password: state.password))
 		}
 
 		let passwordField = Input(initialText: "", placeholder: "Password") { str in
-			component.replaceState(DemoState2(username: state.username, password: str))
+			component.replaceState(LogInState(username: state.username, password: str))
 		}
 
 		let enabled = (state.username.utf16Count > 0 && state.password.utf16Count > 0)
@@ -36,5 +40,18 @@ class DemoComponent2<S>: Few.Component<DemoState2> {
 
 		let layout = verticalStack(12) >-- offset(CGPoint(x: 12, y: 12))
 		return Container(children: [usernameField, passwordField, loginButton], layout: layout)
+	}
+}
+
+class DemoComponent2<S>: Few.Component<DemoState2> {
+	init() {
+		let initialState = DemoState2(loggedIn: false, logInState: LogInState(username: "", password: ""))
+		super.init(render: DemoComponent2.render, initialState: initialState)
+	}
+
+	class func render(component: Few.Component<DemoState2>, state: DemoState2) -> Element {
+		let login = LogInComponent<LogInState>(state: state.logInState)
+		login.sizingBehavior = .Fixed(CGSize(width: 480, height: 360))
+		return login
 	}
 }
