efdc0aaa49d8e113aaa26ed9f350a59a5f5e7d8c
joshaber <joshaber@gmail.com>
2014-11-29 21:55:42 -0500

    Fetch the watcher count.

--- FewDemo/DemoComponent1.swift
+++ FewDemo/DemoComponent1.swift
@@ -9,41 +9,72 @@
 import Foundation
 import Few
 
+func GET(URL: NSURL, fn: (NSDictionary!, NSURLResponse!, NSError!) -> ()) {
+	NSURLSession.sharedSession().dataTaskWithURL(URL) { (data, response, error) in
+		if data == nil {
+			fn(nil, response, error)
+			return
+		}
+
+		var JSONError: NSError?
+		let result = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0), error: &JSONError) as? NSDictionary
+		fn(result, response, JSONError)
+	}.resume()
+}
+
 struct DemoState1 {
 	let todos: [String] = []
 	let like = false
+	let watcherCount: Int?
 }
 
 class DemoComponent1<S>: Few.Component<DemoState1> {
 	init() {
-		let initialState = DemoState1(todos: (1...100).map { "Todo #\($0)" }, like: false)
+		let initialState = DemoState1(todos: (1...100).map { "Todo #\($0)" }, like: false, watcherCount: nil)
 		super.init(render: DemoComponent1.render, initialState: initialState)
+	}
+
+	override func componentDidRealize() {
+		let URL = NSURL(string: "https://api.github.com/repos/ReactiveCocoa/ReactiveCocoa")
+		GET(URL!) { (JSON, response, error) in
+			if JSON == nil {
+				println("Error: \(error)")
+				println("Response: \(response)")
+				return
+			}
+
+			let watchers = JSON["watchers_count"] as? Int
+			dispatch_async(dispatch_get_main_queue()) {
+				let state = self.getState()
+				self.replaceState(DemoState1(todos: state.todos, like: state.like, watcherCount: watchers))
+			}
+		}
 	}
 
 	class func render(component: Few.Component<DemoState1>, state: DemoState1) -> Element {
 		let count = Label(text: "\(state.todos.count)")
 
 		let button = Button(title: "Add") {
-			component.replaceState(DemoState1(todos: state.todos + ["a nu todo"], like: state.like))
+			component.replaceState(DemoState1(todos: state.todos + ["a nu todo"], like: state.like, watcherCount: state.watcherCount))
 		}
 
 		let likedness = (state.like ? "do" : "donut")
 		let statusLabel = Label(text: "I \(likedness) like this.")
 
 		let toggleButton = Button(title: "Toggle") {
-			component.replaceState(DemoState1(todos: state.todos, like: !state.like))
+			component.replaceState(DemoState1(todos: state.todos, like: !state.like, watcherCount: state.watcherCount))
 		}
 
-		let likesIt = Label(text: "He likes it he really likes it!")
+		let likesIt = maybe(state.watcherCount, Label(text: "Checking…")) {
+			Label(text: "\($0) people like us!!!")
+		}
 
 		var children = [count, button, statusLabel, toggleButton]
 		if !state.like {
 			children += [likesIt]
 		}
 
-		// The [Element] cast is necessary otherwise Swift loses it shit at runtime.
-		// "NSArray element failed to match the Swift Array Element type"
-		let todos = state.todos.map { Label(text: $0) } as [Element]
+		let todos = state.todos.map { Label(text: $0) }
 		let list = List(todos)
 		list.frame.size = CGSize(width: 100, height: 100)
 		children += [list]
