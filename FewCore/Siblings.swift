5e506def1c74238ee896177f1091f3c1eda812dc
joshaber <joshaber@gmail.com>
2015-01-27 20:38:23 -0500

    Siblings probably doesn't need to be public.

--- FewCore/Siblings.swift
+++ FewCore/Siblings.swift
@@ -12,17 +12,17 @@ public func +(left: Element, right: Element) -> Element {
 	return Siblings(left, right)
 }
 
-public class Siblings: Element {
+internal class Siblings: Element {
 	private let child1: Element
 	private let child2: Element
 
-	public init(_ child1: Element, _ child2: Element) {
+	internal init(_ child1: Element, _ child2: Element) {
 		self.child1 = child1
 		self.child2 = child2
 		super.init(frame: CGRectUnion(child1.frame, child2.frame))
 	}
 
-	public required init(copy: Element, frame: CGRect, hidden: Bool, alpha: CGFloat, key: String?) {
+	internal required init(copy: Element, frame: CGRect, hidden: Bool, alpha: CGFloat, key: String?) {
 		let container = copy as Siblings
 		child1 = container.child1
 		child2 = container.child2
@@ -31,7 +31,7 @@ public class Siblings: Element {
 
 	// MARK: Element
 
-	public override func getChildren() -> [Element] {
+	internal override func getChildren() -> [Element] {
 		return [ child1, child2 ]
 	}
 }
