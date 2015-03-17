66ea767517cbc64901262ed6cdbb88350abcd9b8
Coen Wessels <c.dewessels@gmail.com>
2015-03-17 10:00:20 +0100

    Deleted the isDefault property from UIButton

--- Few-iOS/Button.swift
+++ Few-iOS/Button.swift
@@ -29,13 +29,11 @@ private class FewButton: UIButton {
 public class Button: Element {
     public var title: NSAttributedString
     public var enabled: Bool
-    public var isDefault: Bool
     private var action: () -> ()
     
-    public init(title: String, enabled: Bool = true, isDefault: Bool = false, action: () -> () = { }) {
+    public init(title: String, enabled: Bool = true, action: () -> () = { }) {
         self.title = NSAttributedString(string: title)
         self.enabled = enabled
-        self.isDefault = isDefault
         self.action = action
         
         super.init(frame: CGRect(x: 0, y: 0, width: 50, height: 23))
