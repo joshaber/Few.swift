4da9de5171b8ce22507fa7dd7e4d0324218d5a5d
joshaber <joshaber@gmail.com>
2015-04-17 19:30:56 -0400

    Update iOS + playground.

--- FewDemo/MyPlayground.playground/Contents.swift
+++ FewDemo/MyPlayground.playground/Contents.swift
@@ -4,5 +4,16 @@ import Cocoa
 import Few
 import XCPlayground
 
-let view = View(backgroundColor: NSColor.redColor()).size(100, 100)
-view.ql
+let view = View(backgroundColor: NSColor.redColor())
+	.direction(.Column)
+	.justification(.FlexEnd)
+	.children([
+		Label("Bleh").size(100, 23),
+		Label("World").size(100, 23),
+		Button(title: "Yoo").margin(Edges(uniform: 4))
+	])
+let component = Component(initialState: ()) { _, _ in view }
+
+let host = NSView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+component.addToView(host)
+host
