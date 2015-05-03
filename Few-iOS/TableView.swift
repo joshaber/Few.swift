6a84fb6f3dc870ffac4b4aa0401f1e4b893fd138
Adlai Holler <guy.on.internet@icloud.com>
2015-05-02 23:32:02 -0700

    iOS: Add TableView.header

--- Few-iOS/TableView.swift
+++ Few-iOS/TableView.swift
@@ -92,11 +92,13 @@ public class TableView: Element {
     private let elements: [Element]
     private let selectionChanged: (Int -> ())?
     private let selectedRow: Int?
-    
-    public init(_ elements: [Element], selectedRow: Int? = nil, selectionChanged: (Int -> ())? = nil) {
+	private let header: Element?
+	
+	public init(_ elements: [Element], selectedRow: Int? = nil, header: Element? = nil, selectionChanged: (Int -> ())? = nil) {
         self.elements = elements
         self.selectionChanged = selectionChanged
         self.selectedRow = selectedRow
+		self.header = header
     }
     
     // MARK: -
@@ -108,12 +110,33 @@ public class TableView: Element {
             let handler = tableView.handler
             
             layoutElements()
-			
+            
             handler?.elements = elements
             
-            let tableView = tableView.handler?.tableView
-            
+            let newHeaderView = header?.realize(realizedSelf).view
+            var needsUpdate = false
+            if newHeaderView != tableView.tableHeaderView {
+                needsUpdate = true
+            } else if let newHeight = newHeaderView?.frame.height where newHeight != tableView.rectForSection(0).minY {
+                needsUpdate = true
+            }
+            if needsUpdate {
+                tableView.tableHeaderView = newHeaderView
+				/// if you don't do this, the table view may compute the wrong header height
+                UIView.performWithoutAnimation {
+                    tableView.beginUpdates()
+                    tableView.endUpdates()
+                }
+            }
+
             handler?.selectionChanged = selectionChanged
+        }
+    }
+    
+    public override func elementDidRealize(realizedSelf: RealizedElement) {
+        super.elementDidRealize(realizedSelf)
+        if let tableView = realizedSelf.view as! FewTableView? {
+            tableView.tableHeaderView = header?.realize(realizedSelf).view
         }
     }
     
