d0c6387ca0a7d338beb118339de3abffa1d37e8d
Adlai Holler <guy.on.internet@icloud.com>
2015-07-06 21:48:18 -0400

    revert Mac changes from previous commit

--- Few-Mac/TableView.swift
+++ Few-Mac/TableView.swift
@@ -12,22 +12,19 @@ import AppKit
 private let defaultRowHeight: CGFloat = 42
 
 private class FewListCell: NSTableCellView {
-	private lazy var parentElement: RealizedElement = {[unowned self] in
-		return RealizedElement(element: Element(), view: self, parent: nil)
-	}()
 	private var realizedElement: RealizedElement?
 
-	func updateWithElement(element: Element) {
-		if let oldRealizedElement = realizedElement {
-			if element.canDiff(oldRealizedElement.element) {
-				element.applyDiff(oldRealizedElement.element, realizedSelf: oldRealizedElement)
+	func updateWithElement(element: Element, parent: RealizedElement) {
+		if let realizedElement = realizedElement {
+			if element.canDiff(realizedElement.element) {
+				element.applyDiff(realizedElement.element, realizedSelf: realizedElement)
 			} else {
-				oldRealizedElement.remove()
+				realizedElement.remove()
 
-				self.realizedElement = element.realize(parentElement)
+				self.realizedElement = element.realize(parent)
 			}
 		} else {
-			realizedElement = element.realize(parentElement)
+			realizedElement = element.realize(parent)
 		}
 
 		realizedElement?.layoutFromRoot()
@@ -46,6 +43,8 @@ private class TableViewHandler: NSObject, NSTableViewDelegate, NSTableViewDataSo
 	let tableView: NSTableView
 
 	var supressChangeNotification = false
+
+	var parents: [String: RealizedElement] = [:]
 
 	var elements: [Element] {
 		didSet {
@@ -88,12 +87,20 @@ private class TableViewHandler: NSObject, NSTableViewDelegate, NSTableViewDataSo
 			let newListCell = FewListCell(frame: CGRectZero)
 			newListCell.identifier = key
 
+			let parent = RealizedElement(element: Element(), view: newListCell, parent: nil)
+			parents[parentKeyForCell(newListCell)] = parent
+
 			listCell = newListCell
 		}
 
-		listCell?.updateWithElement(element)
+		let parent = parents[parentKeyForCell(listCell!)]!
+		listCell?.updateWithElement(element, parent: parent)
 
 		return listCell
+	}
+
+	func parentKeyForCell(cell: FewListCell) -> String {
+		return memoryAddress(cell)
 	}
 
 	@objc func tableView(tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
